59c1d9efa73a9e9c2872b2e923eb637dafd8726b
Alejo Flores Lucey <alejo@misterflores.com>
2021-07-06 18:35:01 -0300

    Changed struct Queries location

--- front.h
+++ front.h
@@ -12,6 +12,13 @@
 #include "queries/query3ADT.h"
 #include "queries/query4ADT.h"
 
+typedef struct {
+    query1ADT q1;
+    query2ADT q2;
+    query3ADT q3;
+    query4ADT q4;
+} Queries;
+
 void handleMemoryError(ERROR_CODE k);
 ERROR_CODE getlines(FILE * file, Queries * queries); // Función usada para parsear el archivo
 ERROR_CODE writeQ4(DataQ4* data, FILE *f); // Función usada para escribir el archivo. Abrir con APPEND
