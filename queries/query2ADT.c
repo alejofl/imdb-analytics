0650287daa288b33b968ca369816b228386dc73e
marianoagopian <agopian.marian@gmail.com>
2021-07-06 13:54:11 -0300

    progreso en querry2ADT, no está terminado

--- queries/query2ADT.c
+++ queries/query2ADT.c
@@ -2,19 +2,111 @@
 
 typedef struct genre
 {
-    String genre;
-    size_t count;
-    struct genre * next;
+    String genre; //el genero
+    size_t count; //cantidad de películas con ese genero
+    struct genre * next; //tail al siguiente genero
 } Genre;
 
 typedef struct year
 {
-    size_t year;
-    Genre * genre;
-    struct year * next;
+    size_t year; //El año en el que estoy
+    Genre * genre; //una lista donde cada nodo es una estructura que posee el genero y la cantidad de peliculas con ese genero
+    struct year * next; //tail al siguiente año
 } Year;
 
 typedef struct query2CDT
 {
-    Year * years;
+    size_t cantGenres; //para saber la cantidad total de generos, para luego hacer el vector que devolvemos con mas facilidad
+    Year * years; /*Una lista donde el nodo contiene el año, una lista con los generos
+                    y la tail al siguiente*/
 } query2CDT;
+
+//Creo el tad para hacer el query2
+query2ADT newQuery2(ERROR_CODE *error){
+    //hago un flag para ver si cuando hago el calloc me tira un error o no
+    errno = 0;
+    //asigno la meemoria para la nueva query
+    query2ADT new = calloc(1, sizeof(query2CDT));
+    //checkeo a ver si el flag me tiro error, si lo hace devuelvo null y devuelvo como parametro que hubo error
+    if(errno == ENOMEM){
+        *error = MEM_ERROR;
+        return NULL;
+    }
+    //en el puntero pongo que no hubo error y retorno el nuevo tad
+    *error = NO_ERROR;
+    return new;
+}
+
+
+static Genre * insertGenreRec(Genre * first, Movie * m, char *addedGenre, ERROR_CODE *error){
+    //creo una variable c para no hacer el strcasecmp varias veces
+    int c;
+    //Si estaba vacío o si ya tenía que estar y no está lo agrego
+    if(first == NULL || (c = strcasecmp(first->genre, m->genres)) > 0){
+        errno = 0;
+        Genre * new = malloc(sizeof(Genre));
+        //Hago las validaciones a ver si no hubo ningun problema con la memoria
+        if(errno == ENOMEM){
+            *error = MEM_ERROR;
+            return first;
+        }
+        //Hago el malloc con el strlen asi le guardo la memoria justa para el nombre del género
+        new->genre = malloc(strlen(m->primaryTitle) + 1);
+        //Valido nuevamente pero esta vez para el string
+        if(errno == ENOMEM){ //PREGUNTAR
+            *error = MEM_ERROR;
+            return first;
+        }
+        //copio el string de género, pongo el tail hacia el first
+        //Inicio el count en 1 ya que por lo menos tengo una película y pongo el flag
+        //added genre en 1 para ya saber que lo agregué
+        strcpy(new->genre, m->genres);
+        new->next = first;
+        new->count = 1;
+        *addedGenre = 1;
+        return new;
+    }
+    if( c == 0){
+        //si el género ya esta solo sumo 1 al count y addedgenre no lo toco
+        //porque lo inicialicé en 0
+        first->count++;
+        return first;
+    }
+    //sigo buscando
+    first->next = insertGenreRec(first->next, m, addedGenre, error);
+    return first;
+}
+
+static Year * insertYearRec(Year * year, Movie * m, char *addedGenre, ERROR_CODE *error){
+    if (year == NULL || year->year < m->startYear){
+        //pongo el flag para ver si hay 
+        errno = 0;
+        Year * new = malloc(sizeof(Year));
+        if(errno == ENOMEM){
+            *error = MEM_ERROR;
+            return year;
+        }
+        // inserto el nuevo año y luego le agrego el género
+        new->year = m->startYear;
+        new->next = year;
+        new->genre =  insertGenreRec(year->genre, m, addedGenre, error);
+        return new;
+    }
+    if (year->year == m->startYear){
+        //Si el año ya está pongo directo el genero
+        year->genre =  insertGenreRec(year->genre, m, addedGenre, error);
+        return year;
+    }
+    //sigo buscando el año
+    year->next = insertYearRec(year->next, m, addedGenre, error);
+    return year;
+}
+
+void insertQ2(query2ADT q, Movie * m, ERROR_CODE *error){
+    //Me fijo si se hizo un nuevo genero
+    char addedGenre = 0;
+    //Meto el nuevo año
+    q->years = insertYearRec(q->years, m, &addedGenre, error);
+    //Aumento la cantidad de generos totales
+    q->cantGenres += addedGenre;
+}
--- queries/query2ADT.h
+++ queries/query2ADT.h
@@ -13,11 +13,11 @@ typedef struct dataQ2
 
 typedef struct query2CDT * query2ADT;
 
-query2ADT newQuery2(void);
+query2ADT newQuery2(ERROR_CODE *error);
 
-void insertQ2(query2ADT q, Movie m);
+void insertQ2(query2ADT q, Movie*  m, ERROR_CODE *error);
 
-DataQ2 * finalVecQ2(query2ADT q);       // Devuelve un vector de estructuras de DataQ2
+DataQ2 * finalVecQ2(query2ADT q, ERROR_CODE *error);       // Devuelve un vector de estructuras de DataQ2
 
 void freeQueryQ2(query2ADT q);
 
