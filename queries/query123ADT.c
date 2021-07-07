d8f49803c9e3ca5e1223da9711761612cbf095ee
sh1ft <fedeshih@gmail.com>
2021-07-07 19:33:23 -0300

    Refactoring query 1 2 3 for more efficiency

---
+++ queries/query123ADT.c
@@ -0,0 +1,97 @@
+#include "query123ADT.h"
+
+typedef struct Recording {
+    String title;
+    size_t votes;
+    float rating;
+} Recording;
+
+typedef struct genre
+{
+    String genre; //el genero
+    size_t count; //cantidad de películas con ese genero
+    struct genre * next; //tail al siguiente genero
+} Genre;
+
+typedef struct year
+{
+    size_t year;        // Almacena el año
+    size_t movies;      // Almacena la cantidad de pelis en un año
+    size_t series;      // Almacena la cantidad de series en un año
+
+    Recording maxMovie;
+    Recording maxSerie;
+
+    Genre * genre;
+    struct year * next; // Apunta al siguiente nodo
+} Year;
+
+typedef struct query123CDT
+{
+    size_t cantYears;
+    size_t cantGenres;        // Indica la cantidad de años registrados en el csv
+    Year * years;       // Una lista donde cada nodo reperesenta un año con su respectiva cantidad de peliculas
+                        // y series
+} query123CDT;
+
+static void insertQ1(Year *year, Entry* entry, ERROR_CODE *err) {
+
+}
+
+static void insertQ2(Year *year, Entry* entry, ERROR_CODE *err, int *addedGenre) {
+
+}
+
+static void insertQ3(Year *year, Entry* entry, ERROR_CODE *err) {
+
+}
+
+static void insertAll(Year *year, Entry* m, ERROR_CODE *error, int *addedGenre) {
+    insertQ1(year, m, error);
+    if (*error != NO_ERROR) {
+        return;
+    }
+    insertQ2(year, m, error, addedGenre);
+    if (*error != NO_ERROR) {
+        return;
+    }
+    insertQ3(year, m, error);
+    if (*error != NO_ERROR) {
+        return;
+    }
+}
+
+static Year * insertYearRec(Year * year, Entry * m, ERROR_CODE *error, char *addedYear, int *addedGenre){
+    if (year == NULL || year->year < m->startYear){
+        //pongo el flag para ver si hay 
+        errno = 0;
+        Year * new = calloc(1, sizeof(Year));
+        if(errno == ENOMEM){
+            *error = MEM_ERROR;
+            return year;
+        }
+        new->year = m->startYear;
+        new->next = year;
+        *addedYear = 1;
+
+        insertAll(new, m, error, addedGenre);
+        return new;
+    }
+    if (year->year == m->startYear){
+        insertAll(year, m, error, addedGenre);
+        return year;
+    }
+    //sigo buscando el año
+    year->next = insertYearRec(year->next, m, error, addedYear, addedGenre);
+    return year;
+}
+
+void insertQ123(query123ADT q, Entry * m, ERROR_CODE *error) {
+    //Me fijo si se hizo un nuevo genero
+    int addedGenre = 0;
+    char addedYear = 0;
+
+    q->years = insertYearRec(q->years, m, error, &addedYear, &addedGenre);
+    //Aumento la cantidad de generos totales
+    q->cantGenres += addedGenre;
+}
---
+++ queries/query123ADT.h
@@ -0,0 +1,59 @@
+#ifndef __Q123ADT_H__
+
+#define __Q123ADT_H__
+
+#include "../queries.h"
+
+typedef struct query123CDT * query123ADT;
+
+typedef struct dataQ1
+{
+    size_t year;
+    size_t movies;
+    size_t series;
+} DataQ1;
+
+typedef struct dataQ2
+{
+    size_t year;
+    String genre;
+    size_t count;
+} DataQ2;
+
+typedef struct dataQ3{
+    size_t year;
+    String movieTitle;
+    size_t movieVotes;
+    float movieRating;
+    String serieTitle;
+    size_t serieVotes;
+    float serieRating;
+} DataQ3;
+
+void insertQ123(query123ADT q, Entry * m, ERROR_CODE *error);
+size_t yearCount(const query123ADT q);
+size_t genresCount(const query123ADT q);
+
+/*
+        Crea el vector con todos los elementos del csv principal ya agregados al TAD
+        @param q: La instancia de query1
+        @param status: Setea el flag con algun valor del enum ERROR_CODE en caso de error en ejecucion
+        @return: El vector ordenado por año de manera descendiente
+ */
+DataQ1 * finalVecQ1(const query123ADT q, ERROR_CODE * status);
+
+/*
+        Libera los recursos del vector final del heap
+        @param vec: El vector obtenido en finalVecQ1
+ */
+void freeFinalVecQ1(DataQ1 * vec);
+
+DataQ2 * finalVecQ2(const query123ADT q, ERROR_CODE *error);       // Devuelve un vector de estructuras de DataQ2
+void freeFinalVecQ2(DataQ2 * vec, size_t dim);
+
+DataQ3 * finalVecQ3(const query123ADT q, ERROR_CODE * err);       // Devuelve un vector de estructuras de DataQ3
+void freeFinalVecQ3(DataQ3 * vec, size_t dim);
+
+void freeQuery123(query123ADT q);
+
+#endif
