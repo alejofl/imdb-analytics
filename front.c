61a68871d4202dfe49cbcff984805bd01f75a73b
Axel <axel.preitit@gmail.com>
2021-07-06 21:36:39 -0300

    getlines algorithm changed

--- front.c
+++ front.c
@@ -50,6 +50,7 @@ String * makeGenresVector(String s, ERROR_CODE * err) {
     return rta;
 }
 
+/*
 ERROR_CODE getlines(FILE * file, Queries * queries) {
     errno = 0;
     char * s = malloc(MAX_LINE);
@@ -146,6 +147,106 @@ ERROR_CODE getlines(FILE * file, Queries * queries) {
     }
     return NO_ERROR;
 }
+*/
+
+// Libera los strings que se almacena en una variable de tipo Entry
+void freeRecordingStrings(Entry * recording){
+    free(recording->titleType);
+    free(recording->primaryTitle);
+    // Debo liberar todos los generos guardados (cada uno de ellos es un string)
+    for(size_t i = 0; recording->genres[i] != NULL; i++) {
+        free(recording->genres[i]);
+    }
+    free(recording->genres);
+}
+
+ERROR_CODE getlines(FILE * file, Queries * queries) {
+    Entry recording;
+    errno = 0;
+    char * s = malloc(MAX_LINE);
+    if (errno == ENOMEM) {
+        return MEM_ERROR;
+    }
+    int c, heading = 1, i = 0;
+    while ((c = fgetc(file)) != EOF) {
+        if (c != '\n') {
+            s[i] = c;
+            i++;
+        } else {
+            s[i] = '\0';
+            i = 0;
+            if(heading == 1){
+                heading = 0;
+            } else {
+                char * token;
+                char separator[2] = ";";
+                int column = 1;
+                ERROR_CODE err;
+                token = strtok(s, separator);
+                while (token != NULL) {
+                    //TODO aca falta chequear lo que devuelva copyStr cuando se arregle
+                    switch (column) {
+                        case 1:
+                            recording.titleType = copyStr(token);
+                            if (recording.titleType == NULL) {
+                                return MEM_ERROR;
+                            }
+                            break;
+                        case 2:
+                            recording.primaryTitle = copyStr(token);
+                            if (recording.primaryTitle == NULL) {
+                                return MEM_ERROR;
+                            }
+                            break;
+                        case 3:
+                            recording.startYear = atol(token);
+                            break;
+                        case 4:
+                            if (strcmp(token, "\\N") == 0) {
+                                recording.endYear = 0;
+                            } else {
+                                recording.endYear = atol(token);
+                            }
+                            break;
+                        case 5:
+                            // Estas llaves son usadas para poder declarar variables en este case.
+                            {
+                                String * genres = makeGenresVector(token, &err);
+                                if (err == MEM_ERROR) {
+                                    return MEM_ERROR;
+                                }
+                                recording.genres = genres;
+                            }
+                            break;
+                        case 6:
+                            recording.averageRating = atof(token);
+                            break;
+                        case 7:
+                            recording.numVotes = atol(token);
+                            break;
+                        case 8:
+                            if (strcmp(token, "\\N") == 0) {
+                                recording.runtimeMinutes = 0;
+                            } else {
+                                recording.runtimeMinutes = atol(token);
+                            }
+                            // TODO falta hacer esta función y despues también hacer lo del MEM_ERROR.
+                            // insertQueries(&recording, queries);
+                            break;
+                        default:
+                            break;
+                    }
+                    column++;
+                    token = strtok(NULL, separator);
+                }
+                // Libera los strings guardados en la estructura del recording para poder reutilizar la variable
+                freeRecordingStrings(&recording);
+            }
+        }
+    }
+    return NO_ERROR;
+}
+
 
 void handleMemoryError(ERROR_CODE k) {
     if (k == MEM_ERROR) {
