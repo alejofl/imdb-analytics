f649f06c4e58c0cc72b7cf19fa2b6c5934146067
Alejo Flores Lucey <alejo@misterflores.com>
2021-07-07 16:28:37 -0300

    Removed old getlines

--- front.c
+++ front.c
@@ -50,105 +50,6 @@ String * makeGenresVector(String s, ERROR_CODE * err) {
     return rta;
 }
 
-/*
-ERROR_CODE getlines(FILE * file, Queries * queries) {
-    errno = 0;
-    char * s = malloc(MAX_LINE);
-    if (errno == ENOMEM) {
-        return MEM_ERROR;
-    }
-    int c, heading = 1, i = 0;
-    while ((c = fgetc(file)) != EOF) {
-        if (c != '\n') {
-            s[i] = c;
-            i++;
-        } else {
-            s[i] = '\0';
-            i = 0;
-
-            char * token;
-            char separator[2] = ";";
-            int column = 1;
-            ERROR_CODE err;
-            Entry * recording = malloc(sizeof(Entry));
-            token = strtok(s, separator);
-            while (token != NULL) {
-                if (!heading) {
-                    //TODO aca falta chequear lo que devuelva copyStr cuando se arregle
-                    switch (column) {
-                        case 1:
-                            recording->titleType = copyStr(token);
-                            if (recording->titleType == NULL) {
-                                return MEM_ERROR;
-                            }
-                            column++;
-                            break;
-                        case 2:
-                            recording->primaryTitle = copyStr(token);
-                            if (recording->primaryTitle == NULL) {
-                                return MEM_ERROR;
-                            }
-                            column++;
-                            break;
-                        case 3:
-                            recording->startYear = atol(token);
-                            column++;
-                            break;
-                        case 4:
-                            if (strcmp(token, "\\N") == 0) {
-                                recording->endYear = 0;
-                            } else {
-                                recording->endYear = atol(token);
-                            }
-                            column++;
-                            break;
-                        case 5:
-                            // Estas llaves son usadas para poder declarar variables en este case.
-                            {
-                                String * genres = makeGenresVector(token, &err);
-                                if (err == MEM_ERROR) {
-                                    return MEM_ERROR;
-                                }
-                                recording->genres = genres;
-                                column++;
-                             }
-                            break;
-                        case 6:
-                            recording->averageRating = atof(token);
-                            column++;
-                            break;
-                        case 7:
-                            recording->numVotes = atol(token);
-                            column++;
-                            break;
-                        case 8:
-                            if (strcmp(token, "\\N") == 0) {
-                                recording->runtimeMinutes = 0;
-                            } else {
-                                recording->runtimeMinutes = atol(token);
-                            }
-                            //TODO falta hacer esta función y despues también hacer lo del MEM_ERROR.
-//                            insertQueries(recording, queries);
-                            column = 1;
-                            break;
-                        default:
-                            break;
-                    }
-                } else {
-                    if (column < 8) {
-                        column++;
-                    } else {
-                        heading = 0;
-                    }
-                }
-                token = strtok(NULL, separator);
-            }
-        }
-    }
-    return NO_ERROR;
-}
-*/
-
 // Libera los strings que se almacena en una variable de tipo Entry
 void freeRecordingStrings(Entry * recording){
     free(recording->titleType);
@@ -327,8 +228,6 @@ ERROR_CODE writeQ3(DataQ3* data, FILE *f) {
     }
     return NO_ERROR;
 }
-
-
 
 ERROR_CODE loadAllQueries(Queries *q) {
     ERROR_CODE k = NO_ERROR;
